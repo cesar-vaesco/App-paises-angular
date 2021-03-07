0ad983db8235349705acbb77619eb1e65137ad8a
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-03-07 09:54:03 -0600

    Paises: creando servicio para construir la url para buscar paises por termino

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -6,13 +7,20 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class PorPaisComponent {
-  constructor() {}
 
-//   Vinculando la propiedad termino a la vista a través de ngModel
-  termino: string = '';
+    //   Vinculando la propiedad termino a la vista a través de ngModel
+    termino: string = '';
+
+    constructor( private PaisService: PaisService) {}
+
 
 //   Acción que se genera al enviar el formulario
   buscar() {
     console.log(this.termino);
+
+    this.PaisService.buscarPais( this.termino )
+    .subscribe( resp =>{
+        console.log(resp);
+    });
   }
 }
---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PaisService {
+    // Guardando la url que se mantendra estatica
+  private apiUrl: string = 'https://restcountries.eu/rest/v2';
+
+  constructor(private http: HttpClient) {}
+
+//   Construyendo la url por término d e búsqueda
+  buscarPais(termino: string): Observable<any> {
+    const url = `${this.apiUrl}/name/${termino}`;
+    return this.http.get(url);
+  }
+}
