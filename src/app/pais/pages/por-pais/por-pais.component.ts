4eb322bc295f21e4b50f91e25037e336cad28ec7
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-03-20 12:46:02 -0600

    Paises: Manejo de errores - mostrando error por ingreso de término equivocado

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -2,25 +2,31 @@ import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
-  selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html',
-  styles: [],
+    selector: 'app-por-pais',
+    templateUrl: './por-pais.component.html',
+    styles: [],
 })
 export class PorPaisComponent {
 
     //   Vinculando la propiedad termino a la vista a través de ngModel
     termino: string = '';
+    hayError: boolean = false;
 
-    constructor( private PaisService: PaisService) {}
+    constructor(private PaisService: PaisService) { }
 
 
-//   Acción que se genera al enviar el formulario
-  buscar() {
-    console.log(this.termino);
+    //   Acción que se genera al enviar el formulario
+    buscar() {
+        this.hayError = false;
+        console.log(this.termino);
+        this.PaisService.buscarPais(this.termino)
+            .subscribe((resp) => {
+                console.log(resp);
 
-    this.PaisService.buscarPais( this.termino )
-    .subscribe( resp =>{
-        console.log(resp);
-    });
-  }
+            }, (err) => {
+                console.log('Error');
+                console.info(err);
+                this.hayError = true;
+            });
+    }
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class PaisService {
     // Guardando la url que se mantendra estatica
-  private apiUrl: string = 'https://restcountries.eu/rest/v2';
+    private apiUrl: string = 'https://restcountries.eu/rest/v2';
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
-//   Construyendo la url por término d e búsqueda
-  buscarPais(termino: string): Observable<any> {
-    const url = `${this.apiUrl}/name/${termino}`;
-    return this.http.get(url);
-  }
+    //   Construyendo la url por término d e búsqueda
+    buscarPais(termino: string): Observable<any> {
+        const url = `${this.apiUrl}/name/${termino}`;
+        return this.http.get(url);
+    }
 }
