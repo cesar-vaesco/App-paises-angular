c4dc1e4e0cf410bead2fda79fc889460d763aee0
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-03-22 13:27:52 -0600

    Paises: Mostrar paises por región: service - componente(.html - .ts)

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
-  selector: 'app-por-region',
-  templateUrl: './por-region.component.html',
-  styles: [
-  ]
+    selector: 'app-por-region',
+    templateUrl: './por-region.component.html',
+    styles: [
+    ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent  {
 
-  constructor() { }
+    regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+    regionActiva:string = '';
+    paises:Country[] = [];
+    constructor( private paisService: PaisService) { }
 
-  ngOnInit(): void {
-  }
+    activarRegion(region: string){
+
+        if( region === this.regionActiva){ return;}
+
+        this.regionActiva = region;
+        this.paises = [];
+        //TODO: hacer el llamado al servicio
+        this.paisService.buscarRegion( region )
+        .subscribe( paises => this.paises = paises);
+    }
+
+    getClaseCSS(region:string):string{
+        return (region === this.regionActiva) ? 'btn btn-dark' : ' btn btn-outline-dark ';
+    }
 
 }
